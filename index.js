3fe8ef65d6d200e14146aae532447f6bb6db2a4b
Swetha-Hirge <swethahirge61@gmail.com>
2023-01-27 09:15:25 +0530

    updated port

--- index.js
+++ index.js
@@ -3,33 +3,40 @@ var request = require('request');
 var app = express();
 const IPData = require("ipdata").default;
 require('dotenv').config();
-
 const apiKey = "e52b05757ad1ed4a66cc621d9e09af5f33256b8273fc9421805b5bc3";
+// const apiKey = process.env.API_KEY;
 const ipdata = new IPData(apiKey);
 
-app.get('/', (req, res) => {
-    request.get('http://ipinfo.io/ip', (error, response, body) => {
-        if (!error && response.statusCode == 200) {
-            let clientIp =  body
-            console.log(body);
-            ipdata.lookup(clientIp)
-                .then((data) => {
-                    console.log(data);
-                    return res.json({
-                        city: data.city,
-                        region: data.region,
-                        country: data.country_name,
-                        postal: data.postal
+app.get('/location', async (req, res) => {
+    try{
+        request.get('http://ipinfo.io/ip', (error, response, body) => {
+            if (!error && response.statusCode == 200) {
+                let clientIp = req.headers["x-forward-ip"] || body
+                console.log(body);
+                ipdata.lookup(clientIp)
+                    .then((data) => {
+                        console.log(data);
+                        return res.json({
+                            city: data.city,
+                            region: data.region,
+                            country: data.country_name,
+                            postal: data.postal
+                        });
+                    })
+                    .catch((error) => {
+                        console.log(error);
+                        res.status(500).send('Error looking up location');
                     });
-                })
-                .catch((error) => {
-                    console.log(error);
-                    res.status(500).send('Error looking up location');
-                });
-        }
-    });
+            }
+        });
+
+    }catch(error) {
+        console.log(error);
+        res.status(500).send('Error looking up location');
+    }
+   
 });
 
-app.listen(6000, () => {
-    console.log('Server started on port 6000');
+app.listen(8000, () => {
+    console.log('Server started on port 3000');
 });
